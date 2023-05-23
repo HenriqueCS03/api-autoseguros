4b27b6f5ed4ff8a37fb1b9f38900a347b0d5cdcf
HenriqueCS03 <rm94276@fiap.com.br>
2023-05-22 21:16:31 -0300

    Documentação

--- src/main/java/br/com/fiap/seguroautomotivo/controllers/PlanoSeguroController.java
+++ src/main/java/br/com/fiap/seguroautomotivo/controllers/PlanoSeguroController.java
@@ -16,66 +16,86 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.seguroautomotivo.models.PlanoSeguro;
 import br.com.fiap.seguroautomotivo.repository.PlanoSeguroRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-
 
 @RestController
 @RequestMapping("/api/assinar")
-//Controle de requisições e acionamento de classes
+@Tag(name="Plano Seguro")
 public class PlanoSeguroController {
-    
+
     @Autowired
     PlanoSeguroRepository planoSeguroRepository;
 
     @GetMapping
+    @Operation(summary = "Listar todos os planos de seguro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de todos os planos obtida com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     public List<PlanoSeguro> todosOsPlanos() {
         return planoSeguroRepository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<PlanoSeguro> cadastrarPlano(@Valid @RequestBody PlanoSeguro planoSeguro){
+    @Operation(summary = "Cadastrar um plano de seguro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Plano de seguro cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
+    public ResponseEntity<PlanoSeguro> cadastrarPlano(@Valid @RequestBody PlanoSeguro planoSeguro) {
         planoSeguroRepository.save(planoSeguro);
-         return ResponseEntity.status(HttpStatus.CREATED).body(planoSeguro);
+        return ResponseEntity.status(HttpStatus.CREATED).body(planoSeguro);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PlanoSeguro> encontraPlanoPorId(@PathVariable Long id){
-        
+    @Operation(summary = "Obter detalhes de um plano de seguro pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Plano de seguro encontrado"),
+            @ApiResponse(responseCode = "404", description = "Plano de seguro não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
+    public ResponseEntity<PlanoSeguro> encontraPlanoPorId(@PathVariable Long id) {
         var planoEncontrado = planoSeguroRepository.findById(id);
-
         if (planoEncontrado.isEmpty())
             return ResponseEntity.notFound().build();
-
         return ResponseEntity.ok(planoEncontrado.get());
     }
 
-
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar um plano de seguro pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Plano de seguro atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Plano de seguro não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     public ResponseEntity<PlanoSeguro> atualizarPlano(@Valid @PathVariable Long id, @RequestBody PlanoSeguro planoSeguro) {
-
         var planoEncontrado = planoSeguroRepository.findById(id);
-
         if (planoEncontrado.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         planoSeguro.setId(id);
         planoSeguroRepository.save(planoSeguro);
-
         return ResponseEntity.ok(planoSeguro);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Remover um plano de seguro pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Plano de seguro removido com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Plano de seguro não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     public ResponseEntity<PlanoSeguro> removerPlano(@PathVariable Long id) {
-        
-       var planoEncontrado = planoSeguroRepository.findById(id);
-        
-       if(planoEncontrado.isEmpty()){
-            return ResponseEntity.noContent().build();
+        var planoEncontrado = planoSeguroRepository.findById(id);
+        if (planoEncontrado.isEmpty()) {
+            return ResponseEntity.notFound().build();
         }
-
         planoSeguroRepository.delete(planoEncontrado.get());
-        
         return ResponseEntity.noContent().build();
     }
-    
 }
--- src/main/java/br/com/fiap/seguroautomotivo/controllers/ServicoController.java
+++ src/main/java/br/com/fiap/seguroautomotivo/controllers/ServicoController.java
@@ -1,6 +1,5 @@
 package br.com.fiap.seguroautomotivo.controllers;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,69 +20,86 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.seguroautomotivo.models.Servico;
 import br.com.fiap.seguroautomotivo.repository.ServicoRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/Servico")
+@Tag(name="Serviço")
 public class ServicoController {
-    
+
     @Autowired
     ServicoRepository servicoRepository;
 
-    @GetMapping()
+    @GetMapping
+    @Operation(summary = "Listar todos os serviços")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de todos os serviços obtida com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     public List<Servico> todosOsServicos() {
-       return servicoRepository.findAll();
+        return servicoRepository.findAll();
     }
-
-    // @GetMapping
-    // public Page<Servico> todosOsCarros(@RequestParam(required = false) String servicosNome, @PageableDefault(size = 5) Pageable pageable) {
-    //     if(servicosNome == null) 
-    //         return servicoRepository.findAll(pageable);
-    //     return servicoRepository.pesquisarPorNomeServico(servicosNome,pageable); 
-    // }
 
     @PostMapping
-    public ResponseEntity<Servico> cadastrarServico(@Valid @RequestBody Servico servico){
+    @Operation(summary = "Cadastrar um serviço")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Serviço cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Requisição inválida"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
+    public ResponseEntity<Servico> cadastrarServico(@Valid @RequestBody Servico servico) {
         servicoRepository.save(servico);
-         return ResponseEntity.status(HttpStatus.CREATED).body(servico);
+        return ResponseEntity.status(HttpStatus.CREATED).body(servico);
     }
-    
-    @GetMapping("/{id}")
-    public ResponseEntity<Servico> encontraServicoPorId(@PathVariable Long id){
-        
-        var servicoEncontrado = servicoRepository.findById(id);
 
+    @GetMapping("/{id}")
+    @Operation(summary = "Obter detalhes de um serviço pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Serviço encontrado"),
+            @ApiResponse(responseCode = "404", description = "Serviço não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
+    public ResponseEntity<Servico> encontraServicoPorId(@PathVariable Long id) {
+        var servicoEncontrado = servicoRepository.findById(id);
         if (servicoEncontrado.isEmpty())
             return ResponseEntity.notFound().build();
-
         return ResponseEntity.ok(servicoEncontrado.get());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Servico> atualizarServico(@Valid @PathVariable Long id ,@RequestBody Servico servico) {
-
+    @Operation(summary = "Atualizar um serviço pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Serviço atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Serviço não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
+    public ResponseEntity<Servico> atualizarServico(@Valid @PathVariable Long id, @RequestBody Servico servico) {
         var servicoEncontrado = servicoRepository.findById(id);
-
         if (servicoEncontrado.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        servico.setId(id);
+       servico.setId(id);
         servicoRepository.save(servico);
-
         return ResponseEntity.ok(servico);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Remover um serviço pelo ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Serviço removido com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Serviço não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor")
+    })
     public ResponseEntity<Servico> removerServico(@PathVariable Long id) {
-        
-       var servicoEncontrado = servicoRepository.findById(id);
-        
-       if(servicoEncontrado.isEmpty()){
+        var servicoEncontrado = servicoRepository.findById(id);
+        if (servicoEncontrado.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-       
         servicoRepository.delete(servicoEncontrado.get());
-
         return ResponseEntity.noContent().build();
     }
 }
