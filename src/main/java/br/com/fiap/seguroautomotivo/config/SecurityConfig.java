8e7a0b2f13f85cdd39fda34821331a009f15ef20
Henrique Cesar de Souza <101295778+HenriqueCS03@users.noreply.github.com>
2023-09-19 20:21:01 -0300

    Voltando restrição de requisições

--- src/main/java/br/com/fiap/seguroautomotivo/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/seguroautomotivo/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/api/registrar").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+            .anyRequest().authenticated()
             .and()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
